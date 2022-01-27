e901fee435afcf59fba76e91025f777a3500aa2c
Kopec99 <96075101+Kopec99@users.noreply.github.com>
2022-01-27 21:40:02 +0100

    Update DistortionDetection.py

--- DistortionDetection.py
+++ DistortionDetection.py
@@ -3,116 +3,137 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 
-global filepathFirst, filepathsecond, FOVmap
-
-centralImage = "path to central image"
-path = glob.glob("path to the folder")
-
-def main():
-    org = cv.imread(r""+ centralImage +"", cv.IMREAD_GRAYSCALE)
-    FOVmap = np.zeros(org.shape, np.float32)
-    imArr = []
-
-    #caluculating optical flow map for each image
-    for image in path:
-        im1 = cv.imread(r""+ centralImage +"", cv.IMREAD_GRAYSCALE)
-        im2 = cv.imread(r""+ image +"", cv.IMREAD_GRAYSCALE)
-
-        shiftX, shiftY = phaseCorrelation(im1, im2)
-        im2 = Shift(im2, shiftY, shiftX)
-        im1, im2 = Crop(im1, im2, shiftX, shiftY, im1.shape[0], im1.shape[1])
-        flow = opticalFlow(im1, im2)
-
-        addToArray(imArr, flow, org, shiftX, shiftY)
-
-    #assinging pixel values to FOVmap
-    for i in range(0, org.shape[0]):
-        for j in range(0, org.shape[1]):
-            vals = []
-            for im in imArr:
-                if im[i, j] != 0:
-                    vals.append(im[i, j])
-            val = sum(vals)/len(vals)
-
-            FOVmap[i, j] = val * 2
+class ImageAnalysis:
+    # Description: This Class contains methods for lens distortion computation and related functions
     
-    plt.imsave("selected dir", FOVmap)
+    # Method summary:
+    # Computes subpixel offset between images using phase correlation
+    # Args: Two image matrices; Out: Two offset values in pixels
+    @staticmethod
+    def PhaseCorrelation(im1, im2):
+        chY = 0; chX = 0
+        im1 = im1.astype(np.float64); im2 = im2.astype(np.float64)
 
-def addToArray(list, im, org, x, y): 
-    x = round(x)
-    y = round(y)
-    rows = org.shape[0]
-    cols = org.shape[1]
-    if y < 0:
-        rows += y
-        y = 0
-    if x < 0:
-        cols += x
-        x = 0
-    temp = np.zeros(org.shape, np.float32)
-    temp[x:cols, y:rows] = im
-    list.append(temp)
+        if im1.shape[0] > 1200 and im1.shape[1] > 1200:
+            k = int(im1.shape[1]//8)
+            l = int(im1.shape[0]//8)
 
-#optical flow calculation
-def opticalFlow(im1, im2): 
-    flow = cv.calcOpticalFlowFarneback(im1, im2, cv.CV_32FC2, pyr_scale=0.5, levels=1,
-        winsize=15, iterations=3, poly_n=7, poly_sigma=1.5, flags=0)
-    FlowMat = (((flow[:,:,0])**2 + (flow[:,:,1])**2)**(1/2))
-    return FlowMat
+            smallIm1 = cv.resize(im1, (k, l))
+            smallIm2 = cv.resize(im2, (k, l))
 
-#image shift method
-def Shift(img, x, y): 
-    transMat = np.float32([[1, 0, x], [0, 1, y]])
-    dimensions = (img.shape[1], img.shape[0])
-    return cv.warpAffine(img, transMat, dimensions)
+            shift1, response1 = cv.phaseCorrelate(smallIm1, smallIm2)
 
-#image crop method
-def Crop(im1, im2, Xshift, Yshift, shape0, shape1): 
-    Xshift = round(Xshift)
-    Yshift = round(Yshift)
+            chX = round(shift1[1] * -8)
+            chY = round(shift1[0] * -8)
 
-    if Yshift < 0:
-        shape1 += Yshift
-        Yshift = 0
-    if Xshift < 0:
-        shape0 += Xshift
-        Xshift = 0
-    a = im1[Xshift:shape0, Yshift:shape1]
-    b = im2[Xshift:shape0, Yshift:shape1]
-    return a, b
-    
-#phase correlation calculation
-def phaseCorrelation(im1, im2): 
-    chY = 0
-    chX = 0
-    im1 = im1.astype(np.float64)
-    im2 = im2.astype(np.float64)
+            win = 50
+            centX = im1.shape[0]//2
+            centY = im1.shape[1]//2
+            im1 = im1[(centX - win):(centX + win), (centY - win):(centY + win)]
+            im2 = im2[(centX - chX - win):(centX - chX + win), (centY - chY - win):(centY - chY + win)]
 
-    if im1.shape[0] > 1200 and im1.shape[1] > 1200:
-        k = int(im1.shape[1]//8)
-        l = int(im1.shape[0]//8)
+        shift2, response2 = cv.phaseCorrelate(im1, im2)
+        subY, subX = shift2
 
-        smallIm1 = cv.resize(im1, (k, l))
-        smallIm2 = cv.resize(im2, (k, l))
+        phaseShiftY = chY - subY
+        phaseShiftX = chX - subX
 
-        shift1, response1 = cv.phaseCorrelate(smallIm1, smallIm2)
+        return phaseShiftX, phaseShiftY
 
-        chX = round(shift1[1] * -8)
-        chY = round(shift1[0] * -8)
+    # Method summary:
+    # Computes optical flow matrix
+    # Args: Two image matrices; Out: Optical flow matrix
+    @staticmethod
+    def OpticalFlow(im1, im2):
+        flow = cv.calcOpticalFlowFarneback(im1, im2, cv.CV_32FC2, pyr_scale=0.5, levels=1, winsize=15, iterations=3, poly_n=7, poly_sigma=1.5, flags=0)
+        FlowMat = (((flow[:,:,0])**2 + (flow[:,:,1])**2)**(1/2))
+        return FlowMat
 
-        win = 50
-        centX = im1.shape[0]//2
-        centY = im1.shape[1]//2
-        im1 = im1[(centX - win):(centX + win), (centY - win):(centY + win)]
-        im2 = im2[(centX - chX - win):(centX - chX + win), (centY - chY - win):(centY - chY + win)]
+    # Method summary:
+    # Moves image matrix by given values
+    # Args: An image, horizontal shift value and vertical shift value; Out: Shifted image matrix
+    @staticmethod
+    def Shift(im, x, y):
+        transMat = np.float32([[1, 0, x], [0, 1, y]])
+        dimensions = (im.shape[1], im.shape[0])
+        return cv.warpAffine(im, transMat, dimensions)
 
-    shift2, response2 = cv.phaseCorrelate(im1, im2)
-    subY, subX = shift2
+    # __private method
+    # Extracts common areas from images
+    @staticmethod
+    def __Crop(im1, im2, Xshift, Yshift, shape0, shape1): 
+        Xshift = round(Xshift)
+        Yshift = round(Yshift)
 
-    phaseShiftY = chY - subY
-    phaseShiftX = chX - subX
+        if Yshift < 0: shape1 += Yshift; Yshift = 0
+        if Xshift < 0: shape0 += Xshift; Xshift = 0
 
-    return phaseShiftX, phaseShiftY
+        IM1 = im1[Xshift:shape0, Yshift:shape1]
+        IM2 = im2[Xshift:shape0, Yshift:shape1]
+        return IM1, IM2
 
-if __name__ == "__main__":
-    main()
+    # __private method
+    # Creates matrix for distortion averaging
+    @staticmethod
+    def __AddToArray(list, im, org, x, y):
+        x = round(x); y = round(y)
+        rows = org.shape[0]; cols = org.shape[1]
+        if y < 0:
+            rows += y
+            y = 0
+        if x < 0:
+            cols += x
+            x = 0
+        temp = np.zeros(org.shape, np.float32)
+        temp[x:cols, y:rows] = im
+        list.append(temp)
+
+    # __private method
+    # Averages distortion values
+    @staticmethod
+    def __CreateDistortionMatrix(org, FOVmap, imArr):
+        for i in range(0, org.shape[0]):
+            for j in range(0, org.shape[1]):
+                vals = []
+                for im in imArr:
+                    if im[i, j] != 0:
+                        vals.append(im[i, j])
+                    if len(vals) != 0:
+                        val = sum(vals)/len(vals)
+                        FOVmap[i, j] = val
+
+    # Method summary:
+    # Computes distortion between two images
+    # Args: Two image matrices; Out: Distortion matrix
+    @staticmethod
+    def CalcDistortion(im1, im2):
+
+        shiftX, shiftY = ImageAnalysis.PhaseCorrelation(im1, im2)
+        im2 = ImageAnalysis.Shift(im2, shiftY, shiftX)       
+        im1, im2 = ImageAnalysis.__Crop(im1, im2, shiftX, shiftY, im1.shape[0], im1.shape[1])
+
+        flow = ImageAnalysis.OpticalFlow(im1, im2)
+        return flow
+
+    # Method summary:
+    # Computes distortion of FOV
+    # Args: Central image matrix, path to folder containing all the samples; Out: Distortion matrix for central image
+    @staticmethod
+    def FOVDistortion(org, folderpath):
+        path = glob.glob(folderpath)
+        FOVmap = np.zeros(org.shape, np.float32)
+        imArray = []
+
+        for image in path:
+            im1 = org.copy()
+            im2 = cv.imread(r""+ image +"", cv.IMREAD_GRAYSCALE)
+
+            shiftX, shiftY = ImageAnalysis.PhaseCorrelation(im1, im2)
+            im2 = ImageAnalysis.Shift(im2, shiftY, shiftX)       
+            im1, im2 = ImageAnalysis.__Crop(im1, im2, shiftX, shiftY, im1.shape[0], im1.shape[1])
+
+            flow = ImageAnalysis.OpticalFlow(im1, im2)
+            ImageAnalysis.__AddToArray(imArray, flow, org, shiftX, shiftY)
+
+        ImageAnalysis.__CreateDistortionMatrix(org, FOVmap, imArray)
+        return FOVmap
